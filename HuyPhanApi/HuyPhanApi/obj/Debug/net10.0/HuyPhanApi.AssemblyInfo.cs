3d9fb09c5063d2541b2c28081f9387089379cbdc
huytv-2025 <huy.tv@smilepms.com>
2026-01-14 15:13:19 +0700

    Mô tả thay đổi của bạn, ví dụ: Sửa giao diện login đẹp hơn

--- HuyPhanApi/HuyPhanApi/obj/Debug/net10.0/HuyPhanApi.AssemblyInfo.cs
+++ HuyPhanApi/HuyPhanApi/obj/Debug/net10.0/HuyPhanApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HuyPhanApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6a6eb0554e98abbba518f1d41f4e48a176e184d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aa7ec9639eef82ac8f6d09589ce7c39dfbb8f46")]
 [assembly: System.Reflection.AssemblyProductAttribute("HuyPhanApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HuyPhanApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
