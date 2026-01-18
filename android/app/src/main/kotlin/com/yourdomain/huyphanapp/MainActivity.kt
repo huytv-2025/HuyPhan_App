3087ae615d108cfcdacce753cdd0fcc38dbe2c32
huytv-2025 <huy.tv@smilepms.com>
2026-01-18 11:56:22 +0700

    Mô tả thay đổi của bạn, ví dụ: Sửa giao diện login đẹp hơn

---
+++ android/app/src/main/kotlin/com/yourdomain/huyphanapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.yourdomain.huyphanapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,17 +5,13 @@
 import FlutterMacOS
 import Foundation
 
-import app_badge_plus
 import file_selector_macos
-import flutter_local_notifications
 import mobile_scanner
 import printing
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AppBadgePlusPlugin.register(with: registry.registrar(forPlugin: "AppBadgePlusPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
